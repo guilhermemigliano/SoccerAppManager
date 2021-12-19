0e59461626d75aef77d8adfdf8a79b79ff162aef
guilhermemigliano <44805868+guilhermemigliano@users.noreply.github.com>
2021-12-19 09:37:33 -0300

    Problema de renderização na home arrumada no AuthContext.

--- config/AuthContext.js
+++ config/AuthContext.js
@@ -26,9 +26,9 @@ export const AuthProvider = ({ children }) => {
   async function signIn(user, password) {
     if (user == USER && password == PASSWORD) {
       await AsyncStorage.setItem('@auth', JSON.stringify(true))
-      return setIslogged(true)
+      setIslogged(true)
     } else {
-      return Alert.alert('Login incorreto')
+      Alert.alert('Login incorreto')
     }
   }
 
